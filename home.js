d071e00312ebe09666fe62f268a46ca5d51ed127
Maxime Lautier <maxime.lautier16@gmail.com>
2025-11-21 13:53:52 +0100

    Ajout note + reset value open vote

--- home.js
+++ home.js
@@ -6,9 +6,19 @@
         const popup = document.getElementById("PopUp");
         const voteBtn = document.getElementById("SoumettreVote");
 
+        //Définition des variable (permetant le reset des catégotie)
+        const Slider = document.getElementById("Note");
+        const Jeu = document.getElementById("VotedGame");
+        const DescriptionJeu = document.getElementById("DescriptionGameVoted")
+        const dateJeu = document.getElementById("DatePlay")
+
         // Quand le bouton est cliqué définir PopUp comme visible
         openBtn.onclick = () => {
             PopUp.style.display = "block";
+            Jeu.value = ""
+            DescriptionJeu.value = ""
+            dateJeu.value = ""
+            Slider.value = 0
         };
 
         // Fermer en cliquant sur la croix
@@ -27,3 +37,16 @@
                 PopUp.style.display = "none";
             }
     };
+
+    // Note sur 5 PopUp
+        // Définition des variable
+            const slider = document.getElementById("Note"); // Définition variable (mise en place chemin d'accès)
+            const AffichageNote = document.getElementById("NoteUser"); // Définition variable (mise en place chemin d'accès)
+
+        // Modification du texte (selon le slider) 
+            slider.addEventListener("input", function () {
+
+                let NoteUser = slider.value / 2; // calcule sur 5
+
+                AffichageNote.textContent = NoteUser + " étoile" + (NoteUser > 1 ? "s" : "") + " sur 5"; // Ajout du s si >1 étoile
+            });
